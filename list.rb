683ba70f76f99b34d439b45e9347b9331dcb33cd
Stephen Gladney <stephengladney@Stephens-Mac-mini.local>
2018-10-16 19:58:29 -0400

    refactor hashes to use symbols, remove hash rockets

--- list.rb
+++ list.rb
@@ -11,14 +11,14 @@ def get_mailchimp_contact_info()
     data = JSON.parse(data)
     contact = data["contact"]
     return {
-        :company => contact["company"],
-        :address1 => contact["addr1"],
-        :city => contact["city"],
-        :state => contact["state"],
-        :zip => contact["zip"],
-        :country => contact["country"],
-        :from_name => "#{data["first_name"]} #{data["last_name"]}",
-        :from_email => data["email"]
+        company: contact["company"],
+        address1: contact["addr1"],
+        city: contact["city"],
+        state: contact["state"],
+        zip: contact["zip"],
+        country: contact["country"],
+        from_name: "#{data["first_name"]} #{data["last_name"]}",
+        from_email: data["email"]
     }
 end
 
@@ -29,23 +29,23 @@ def create_mailchimp_list(name)
     user_contact_info = get_mailchimp_contact_info()
     RestClient.post "https://us19.api.mailchimp.com/3.0/lists", 
     {
-        "name" => name,
-        "contact" => {
-            "company" => user_contact_info[:company],
-            "address1" => user_contact_info[:address1],
-            "city" => user_contact_info[:city],
-            "state" => user_contact_info[:state],
-            "zip" => user_contact_info[:zip],
-            "country" => user_contact_info[:country]
+        "name": name,
+        "contact": {
+            company: user_contact_info[:company],
+            address1: user_contact_info[:address1],
+            city: user_contact_info[:city],
+            state: user_contact_info[:state],
+            zip: user_contact_info[:zip],
+            country: user_contact_info[:country]
         },
-        "permission_reminder" => "you signed up for this bc you love me",
-        "campaign_defaults" => {
-            "from_name" => user_contact_info[:from_name],
-            "from_email" => user_contact_info[:from_email],
-            "subject" => "TBD yall",
-            "language" => "english"
+        "permission_reminder": "you signed up for this bc you love me",
+        "campaign_defaults": {
+            from_name: user_contact_info[:from_name],
+            from_email: user_contact_info[:from_email],
+            subject: "TBD yall",
+            language: "english"
         },
-        "email_type_option" => false
+        email_type_option: false
     }.to_json,
     {
         Authorization: "Bearer " + ENV["MC_AUTH_TOKEN"],
@@ -55,3 +55,5 @@ end
 
 def add_field_to_list(name, type)
 end
+
+create_mailchimp_list("another test")
