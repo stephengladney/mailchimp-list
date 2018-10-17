d62ca073cbb594af73f3fdd4d4672018ff883cc2
Stephen Gladney <stephengladney@Stephens-Mac-mini.local>
2018-10-16 20:36:26 -0400

    add find_mailchimp_list_id_by_name function

--- list.rb
+++ list.rb
@@ -1,9 +1,12 @@
 require 'rest-client'
 require 'json'
 
+LANGUAGE = "english"
+DATA_CENTER = "us19"
+
 
 def get_mailchimp_contact_info()
-    data = RestClient.get "https://us19.api.mailchimp.com/3.0/", 
+    data = RestClient.get "https://#{DATA_CENTER}.api.mailchimp.com/3.0/", 
     {
         Authorization: "Bearer " + ENV["MC_AUTH_TOKEN"],
         Content_Type: "application/json"
@@ -22,14 +25,30 @@ def get_mailchimp_contact_info()
     }
 end
 
-def get_mailchimp_list()
+def find_mailchimp_list_id_by_name(search_string)
+    id = ""
+    data = RestClient.get "https://#{DATA_CENTER}.api.mailchimp.com/3.0/lists", 
+    {
+        Authorization: "Bearer " + ENV["MC_AUTH_TOKEN"],
+        Content_Type: "application/json"
+    }
+    data = JSON.parse(data)
+    lists = data["lists"]
+    found_list = lists.select{|i| i["name"].include? search_string}
+    if found_list.length > 1
+        return "Multiple lists found. Narrow search"
+    elsif found_list.length == 1
+        return found_list[0]["id"]
+    elsif found_list.length == 0
+        return "List not found"
+    end
 end
 
-def create_mailchimp_list(name)
+def create_mailchimp_list(list_name)
     user_contact_info = get_mailchimp_contact_info()
-    RestClient.post "https://us19.api.mailchimp.com/3.0/lists", 
+    RestClient.post "https://#{DATA_CENTER}.api.mailchimp.com/3.0/lists", 
     {
-        "name": name,
+        "name": list_name,
         "contact": {
             company: user_contact_info[:company],
             address1: user_contact_info[:address1],
@@ -43,7 +62,7 @@ def create_mailchimp_list(name)
             from_name: user_contact_info[:from_name],
             from_email: user_contact_info[:from_email],
             subject: "TBD yall",
-            language: "english"
+            language: LANGUAGE
         },
         email_type_option: false
     }.to_json,
@@ -53,7 +72,5 @@ def create_mailchimp_list(name)
     }
 end
 
-def add_field_to_list(name, type)
+def add_field_to_list(list_id, field_name, field_type)
 end
-
-create_mailchimp_list("another test")
